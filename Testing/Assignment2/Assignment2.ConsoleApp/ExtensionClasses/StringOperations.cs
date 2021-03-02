4d0cb98fe58e9892a1fd96bd03749c95d7c6a940
prrince <princemakwana29@gmail.com>
2021-03-03 00:00:00 +0530

    Testing Assignment 2 Added.

---
+++ Testing/Assignment2/Assignment2.ConsoleApp/ExtensionClasses/StringOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.ConsoleApp.ExtensionClasses
+{
+    public static class StringOperations
+    {
+        public static string ChangeCase(this string inputString)
+        {
+            return Char.IsLower(inputString[0]) ? inputString.ToUpper() : inputString.ToLower();
+        }
+    }
+}
---
+++ Testing/Assignment2/Assignment2.Tests/StringOperationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Assignment2.ConsoleApp.ExtensionClasses;
+
+namespace Assignment2.Tests
+{
+    public class StringOperationTest
+    {
+        [Fact]
+        public void Test_ChangeCase()
+        {
+            // Arrange
+            var inputString = "Upper";
+            var outputString = "upper";
+            // Act
+            var newString = inputString.ChangeCase();
+            // Assert
+            Assert.Equal(newString, outputString);
+        }
+    }
+}
